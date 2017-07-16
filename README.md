a0b8b7408cffa4a9c9a65e1ba764ba3a0ffe5251
Travis Weidenbenner <travis@thetravisw.com>
2017-07-15 20:47:57 -0500

    Basic rule implementation.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # middleman
+
+### Route Rule Object (implemented only)
+| Parameter  | Description                                          |
+| ---------- | ---------------------------------------------------- |
+| body       | Body object to send back to client in response       |
+| path       | Apply rule to requests to paths that match           |
+| skipApi    | Skip call to api server and send predefined response |
+| statusCode | Status code to send back to the client               |
--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const path = require('path')
-
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
-const bodyParser = require('body-parser')
 const request = require('request')
 const io = require('socket.io')(server)
+
 
 // Port to run api and web server on
 const PORT = 3000
 
 // Base url for api to proxy to.
 const BASE_URL = 'https://jsonplaceholder.typicode.com'
+
+// Default set of rules (to fall back on if non specified)
+const DEFAULT_RULES = {
+  interceptRequest  :  false, // Allow "breakpoint" on original request before sent to api
+  interceptResponse :  false, // Allow "breakpoint" on response from api before sent to client
+  skipApi           :  false  // Allow skipping going to the api all together
+}
 
 // Express setup
 app.use(bodyParser.json())
@@ -24,20 +31,31 @@ app.use((req, res, next) => {
 // Admin panel setup
 app.use('/middleman', express.static('frontend'))
 
+// Request count to use as IDs for request
+let request_count = 0
+
+// List of rules to activate on routes
+let rules = []
+
 const handleRoute = (req, res) => {
   // API url to make the request to
   const REQUEST_URL = BASE_URL + req.originalUrl
   const HEADERS_TO_KEEP = []
 
-  console.log(`[PROXY][${req.method}] ${REQUEST_URL}`)
+  console.log(`[${++request_count}][PROXY][${req.method}] ${REQUEST_URL}`)
+
+  // Grab rule for given route make match regex in future
+  let route_rule = rules.filter((el) => el.path === req.originalUrl)[0] || DEFAULT_RULES
 
   // Websocket here for request to proxy (before sent to api server)
   io.emit('proxy_request', {
+    request_id: request_count,
     headers: req.headers,
     method: req.method,
     body: req.body,
     host: req.hostname,
-    path: req.originalUrl
+    path: req.originalUrl,
+    rule: route_rule
   })
 
   // Generate headers to keep
@@ -58,23 +76,42 @@ const handleRoute = (req, res) => {
     strictSSL: false
   }
 
-  request(options, (error, response, body) => {
-    console.log(`[${options.method}][${response.statusCode}] ${REQUEST_URL}`)
+  if (!route_rule.skipApi) {
+    request(options, (error, response, body) => {
+      console.log(`[${request_count}][${options.method}][${response.statusCode}] ${REQUEST_URL}`)
 
-    // Websocket here for response from api
+      // Websocket here for response from api
+      io.emit('api_response', {
+        request_id: request_count,
+        headers: response.headers,
+        method: options.method,
+        body: body,
+        url: options.url,
+        status: response.statusCode,
+        apiSkipped: false
+      })
+
+      // Send response as close to api response as possible
+      res.header('Content-Type', response.headers['content-type'])
+      res.status(route_rule.statusCode || response.statusCode)
+      res.json(route_rule.body || body || {})
+    })
+  } else {
     io.emit('api_response', {
-      headers: response.headers,
-      method: options.method,
-      body: body,
+      request_id: request_count,
+      headers: res.headers,
+      method: res.method,
+      body: route_rule.body || {},
       url: options.url,
-      status: response.statusCode
+      status: route_rule.statusCode || 200,
+      apiSkipped: true
     })
 
-    // Send response as close to api response as possible
-    res.header('Content-Type', response.headers['content-type'])
-    res.status(response.statusCode)
-    res.json(body)
-  })
+    res.header('Content-Type', 'application/json')
+    res.status(route_rule.statusCode || 200)
+    res.json(route_rule.body || {})
+  }
+
 }
 
 // Prevent favicon requests
@@ -86,8 +123,11 @@ app.get('/favicon.ico', (req, res) => {
 app.get('*', handleRoute)
 app.post('*', handleRoute)
 
-io.on('connection', function(socket) {
-  // Send current routes being messed with here
+// Send data needed for admin setup on setup
+io.on('connection', (socket) => {
+  socket.emit('settings', {
+    rules: rules
+  })
 })
 
 server.listen(PORT, () => {
