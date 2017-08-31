5dc35e737bdd482090ba24b1e2fa80bc2a813ca9
Travis Weidenbenner <travis@thetravisw.com>
2017-08-30 20:26:53 -0500

    Refactor code into promises

--- src/backend/app.js
+++ src/backend/app.js
@@ -25,153 +25,62 @@ const app = (server) => {
     // Grab rule for given route make match regex in future
     let route_rule = rules.match(ruleList, req)
 
-    // Generate unique IDs for request and responsef
-    let request_id = uuid()
-    let response_id = uuid()
-
-    // Websocket here for request to proxy (before sent to api server)
-    io.emit('proxy_request', {
-      request_id: request_count,
-      headers: req.headers,
-      method: req.method,
-      body: req.body,
-      host: req.hostname,
-      path: req.originalUrl,
+    let originalObj = {
+      id: request_count,
       rule: route_rule,
-      intercepted_id: route_rule.interceptRequest ? request_id : false
-    })
-
-    // Generate headers to send to server
-    let headers = {}
-    let headers_to_keep = route_rule.keep_headers || config.global.keep_headers || []
-
-    for (let i = 0; i < headers_to_keep.length; i++) {
-      if (req.headers.hasOwnProperty(headers_to_keep[i])) {
-        headers[headers_to_keep[i]] = req.headers[headers_to_keep[i]]
+      request: {
+        headers: req.headers,
+        method: req.method,
+        body: req.body,
+        hostName: req.hostName,
+        originalUrl: req.originalUrl
       }
     }
 
-    // Set options to relay to api server
-    let options = {
-      url: REQUEST_URL,
-      method: req.method,
-      headers: headers,
-      data: req.body,
-      strictSSL: false
-    }
+    intercept(originalObj, 'request', io)
+      .then(obj => {
+        // Generate headers to send to server
+        let headers = {}
+        let headers_to_keep = obj.rule.keep_headers || config.global.keep_headers || []
 
-    // interceptRequest if the route rule says to
-    if (route_rule.interceptRequest && io.sockets.sockets.length !== 0) {
-      console.log("Waiting for response from admin")
-      let resolved = false
-
-      for (let id in io.sockets.sockets) {
-        let socket = io.sockets.sockets[id]
-
-        socket.once(request_id, (data) => {
-          if (!resolved) {
-            resolved = true
-            options.body = data.body || options.body
-            options.method = data.method || options.method
+        for (let i = 0; i < headers_to_keep.length; i++) {
+          if (obj.request.headers.hasOwnProperty(headers_to_keep[i])) {
+            headers[headers_to_keep[i]] = obj.request.headers[headers_to_keep[i]]
           }
-        })
-      }
-    }
+        }
 
-    let responseObj = {
-      request_id: request_count,
-      headers: res.headers,
-      method: res.method,
-      body: route_rule.body || {},
-      url: options.url,
-      statusCode: route_rule.statusCode || 200,
-      apiSkipped: !route_rule.interceptResponse,
-      intercepted_id: route_rule.interceptResponse ? response_id : false,
-      rule: route_rule,
-      contentType: 'application/json'
-    }
-
-    if (!route_rule.skipApi) {
-      axios(options).then(response => {
-        console.log(`[${request_count}][${options.method}][${response.status}] ${REQUEST_URL}`)
-        responseObj.headers = response.headers
-        responseObj.body = route_rule.body || response.data
-        responseObj.method = options.method
-        responseObj.statusCode = route_rule.statusCode || response.status
-        responseObj.contentType = response.headers['content-type']
-
-        sendResponse(responseObj, res)
-      }).catch(err => {
-        let response = err.response
-        console.log(`[${request_count}][${options.method}][${response.status}] ${REQUEST_URL}`)
-        responseObj.headers = response.headers
-        responseObj.body = route_rule.body || response.data
-        responseObj.method = options.method
-        responseObj.statusCode = route_rule.statusCode || response.status
-        responseObj.contentType = response.headers['content-type']
-
-        sendResponse(responseObj, res)
+        obj.request.headers = headers
+        
+        return request(obj)
       })
-    } else {
-      // Websocket here for response from api
-      sendResponse(responseObj, res)
-    }
+      .then(obj => {
+        return intercept(obj, 'response', io)
+      })
+      .then(obj => {
+        // return shit here
+        sendResponse(obj.response, res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 
-  const sendResponse = (responseObj, res) => {
-    // Websocket here for response from api
-    io.emit('api_response', responseObj)
-
-    // Send response as close to api response as possible
-    res.header('Content-Type', responseObj.contentType)
-    res.status(responseObj.statusCode)
-
-    // Listen for response from admin if breakpoint on
-    if (responseObj.rule.interceptResponse && io.sockets.sockets.length !== 0) {
-      console.log("Waiting for response from admin")
-      let resolved = false
-
-      for (let id in io.sockets.sockets) {
-        let socket = io.sockets.sockets[id]
-
-        socket.once(responseObj.response_id, (data) => {
-          if (!resolved) {
-            resolved = true
-            res.status(data.statusCode || 200)
-            res.json(data.body || {})
-          }
-        })
-      }
-    } else {
-      // Just send back responses
-      res.json(responseObj.body)
-    }
-  }
-
-  const initSockets = () => {
-    // Send data needed for admin setup on setup
-    io.on('connection', (socket) => {
-      socket.emit('settings', {
-        rules: ruleList
-      })
-
-      socket.on('UPDATE_RULE', (rule) => {
-        const index = ruleList.findIndex(r => r.id === rule.id)
-        ruleList[index] = rule
-      })
-    })
-  }
-
-  const intercept = (obj) => {
+  const intercept = (obj, type, io) => {
     return new Promise((resolve, reject) => {
-      if (io.sockets.sockets.length !== 0) {
-        console.log('Waiting for response from admin')
+      if (obj.rule[`intercept${type.charAt(0).toUpperCase() + type.slice(1)}`] && io.sockets.sockets.length !== 0) {
         let resolved = false
+        let newObj = obj
+
+        newObj.interceptId = uuid()
+
+        // Send event to clients
+        io.emit(`intercept_${type}`, newObj)
+        console.log('Waiting on response from client')
 
         for (let id in io.sockets.sockets) {
           let socket = io.sockets.sockets[id]
 
-          socket.once(obj.intercepted_id, (data) => {
+          socket.once(newObj.interceptId, (data) => {
             if (!resolved) {
               resolved = true
               resolve(data)
@@ -186,25 +95,80 @@ const app = (server) => {
 
   const request = (obj) => {
     return new Promise((resolve, reject) => {
+      const REQUEST_URL = BASE_URL + obj.request.originalUrl
+
+      let newObj = obj
+
+      // Set options to relay to api server
+      let options = {
+        url: REQUEST_URL,
+        method: obj.request.method,
+        headers: obj.request.headers,
+        data: obj.request.body,
+        strictSSL: false
+      }
+
+      let responseObj = {
+        headers: obj.request.headers,
+        method: obj.request.method,
+        body: obj.rule.body || {},
+        url: options.url,
+        statusCode: obj.rule.statusCode || 200,
+        apiSkipped: !obj.rule.interceptResponse,
+        contentType: 'application/json'
+      }
+
       if (!obj.rule.skipApi) {
-        // Generate axios options here
-        let options = {
-
-        }
-
         axios(options).then(response => {
-          // Gen new obj here
+          console.log(`[${request_count}][${options.method}][${response.status}] ${REQUEST_URL}`)
+          responseObj.headers = response.headers
+          responseObj.body = obj.rule.body || response.data
+          responseObj.method = options.method
+          responseObj.statusCode = obj.rule.statusCode || response.status
+          responseObj.contentType = response.headers['content-type']
 
+          newObj.response = responseObj
           resolve(newObj)
         }).catch(err => {
-          // Gen new obj here
+          let response = err.response
+          console.log(`[${request_count}][${options.method}][${response.status}] ${REQUEST_URL}`)
+          responseObj.headers = response.headers
+          responseObj.body = obj.rule.body || response.data
+          responseObj.method = options.method
+          responseObj.statusCode = obj.rule.statusCode || response.status
+          responseObj.contentType = response.headers['content-type']
 
+          newObj.response = responseObj
           resolve(newObj)
         })
       } else {
-        // Skip
-        resolve(obj)
+        newObj.response = responseObj
+        resolve(newObj)
       }
+    })
+  }
+
+  const sendResponse = (responseObj, res) => {
+    // Websocket here for response from api
+    io.emit('api_response', responseObj)
+
+    // Send response as close to api response as possible
+    res.header('Content-Type', responseObj.contentType)
+    res.status(responseObj.statusCode)
+    res.json(responseObj.body)
+  }
+
+  const initSockets = () => {
+    // Send data needed for admin setup on setup
+    io.on('connection', (socket) => {
+      socket.emit('settings', {
+        rules: ruleList
+      })
+
+      socket.on('UPDATE_RULE', (rule) => {
+        const index = ruleList.findIndex(r => r.id === rule.id)
+        ruleList[index] = rule
+      })
     })
   }
 
