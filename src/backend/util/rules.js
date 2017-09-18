5840d48b19972a3f550d98998c6120d91238463f
Travis Weidenbenner <travis@thetravisw.com>
2017-09-17 21:55:26 -0500

    Start JSDoc for rules

--- src/backend/util/rules.js
+++ src/backend/util/rules.js
@@ -1,23 +1,71 @@
 let uuid = require('uuid/v4')
 
+/**
+ * Default values for a hijacker rule
+ *
+ * @constant
+ * @type {Object}
+ * @default
+ */
 const DEFAULT_RULE = {
-  // Allow "breakpoint" on original request before sent to api
-  interceptRequest: false,
-
-  // Allow "breakpoint" on response from api before sent to client
-  interceptResponse: false,
-
-  // Allow skipping going to the api all together
-  skipApi: false,
-
-  // Rule disabled. If true, rule wont be run
+  // TODO: Add all possible rule values here (undefined if need be, will require some rework elswhere)
+  /**
+   * Flag to disable a rule
+   *
+   * @type {boolean}
+   * @inner
+   */
   disabled: false,
 
-  // Route the requested url path to a different one in the api
-  routeTo: undefined
+  /**
+   * Allow "breakpoint" on original request before sent to api
+   *
+   * @type {boolean}
+   * @inner
+   */
+  interceptRequest: false,
+
+  /**
+   * Allow "breakpoint" on response from api before sent to client
+   *
+   * @type {boolean}
+   * @inner
+   */
+  interceptResponse: false,
+
+  /**
+   * Name of route, display purposes only
+   *
+   * @type {string}
+   * @inner
+   */
+  name: undefined,
+
+  /**
+   * Route the requested url path to a different one in the api
+   *
+   * @type {boolean}
+   * @inner
+   */
+  routeTo: undefined,
+
+  /**
+   * Allow skipping going to the api all together
+   *
+   * @type {boolean}
+   * @inner
+   */
+  skipApi: false
 }
 
-const match = function(list, req) {
+/**
+ * Match a reques to a rule from a list
+ *
+ * @param {Array<Object>} list - List of rules to match from
+ * @param {Request} req - Express request object to match
+ * @return {Object} Rule that matches the given request. Default rule if no match
+ */
+const match = (list, req) => {
   return list.filter((rule) => {
     // Make sure path and method match and rule is enabled
     return !rule.disabled && rule.path === req.originalUrl
@@ -25,6 +73,12 @@ const match = function(list, req) {
   })[0] || DEFAULT_RULE
 }
 
+/**
+ * Read in a rule. Apply default values where needed
+ *
+ * @param {Object} rule - Rule to read in and modify if needed
+ * @return {Object} Rule with default values filled in where needed
+ */
 const read = function(rule) {
   // TODO: Validate rule and add functionality to check depricaded functions
   return Object.assign({}, DEFAULT_RULE, rule, { id: uuid() })
