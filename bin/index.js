3b3cde7aa10d0297515af51148b7dc545240c5a7
Travis Weidenbenner <travis@thetravisw.com>
2017-09-30 19:42:37 -0500

    Add commander to start basic cli

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,13 @@
 #!/usr/bin/env node
+
+const program = require('commander')
+
+const pkg = require('../package')
+
+// Define CLI
+program
+  // .option('-c, --config <path>', 'set path to hijacker configuration')
+  .version(pkg.version)
+  .parse(process.argv)
+
 require('../index.js')
