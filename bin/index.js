e134f1699173abdaf4371e7999ffbcd498727c6e
Travis Weidenbenner <travis@thetravisw.com>
2017-11-07 20:20:03 -0600

    Refactor server into main function to make it easier to create new instances.

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
+const path = require('path')
 const program = require('commander')
 
 const pkg = require('../package')
+const hijacker = require('../lib/hijacker')
 
 // Define CLI
 program
@@ -11,4 +13,18 @@ program
   .parse(process.argv)
 
 // TODO: Restructure to take in options as argument
-require('../index.js')
+let rc = {}
+
+// TODO: Figure out a better way to read in rules (and watch?)
+try {
+  // eslint-disable-next-line
+  rc = require(path.join(process.cwd(), `${pkg.name}.config`))
+} catch (e) {
+  // No config file. Create and start again
+  console.error('No config file found: Please set up a config file and start again')
+  process.exit(1)
+}
+const server = hijacker(rc)
+server.on('started', (port) => {
+  console.log(`Application is listening on port ${port}`)
+})
--- index.js
+++ index.js
@@ -1,42 +1,7 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const path = require('path')
-const http = require('http')
+/**
+ * Hijacker
+ * Copyright(c) 2017 Travis Weidenbenner
+ * MIT Licensed
+ */
 
-const config = require('./lib/backend/util/config').config
-const backend = require('./lib/backend/app')
-
-const app = express()
-const server = http.Server(app)
-const hijacker = backend(server)
-
-// Express setup
-app.use(bodyParser.json())
-app.use((req, res, next) => {
-  const configAllow = (config.globalRule.allow_headers || []).join(', ')
-
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', configAllow)
-  next()
-})
-
-// Admin panel setup
-app.use('/hijacker', express.static(path.join(__dirname, 'lib/frontend')))
-
-// Prevent favicon requests
-app.get('/favicon.ico', (req, res) => {
-  res.status(204)
-})
-
-// Set routes for the proxy to lisetn too
-app.delete('*', hijacker.handleRoute)
-app.get('*', hijacker.handleRoute)
-app.patch('*', hijacker.handleRoute)
-app.post('*', hijacker.handleRoute)
-app.put('*', hijacker.handleRoute)
-
-hijacker.initSockets()
-
-server.listen(config.port, () => {
-  console.log(`Application is listening on port ${config.port}`)
-})
+module.exports = require('./lib/hijacker')
