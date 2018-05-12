2ce64e3f96253407a103cfa5560178ffa28c6157
Travis Weidenbenner <travis@thetravisw.com>
2018-05-12 18:51:05 -0500

    refactor(frontend): Start of new frontend
    Scrap old frontend and start basic webpack build

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -1,18 +1,1 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-import VueRouter from 'vue-router'
-
-import store from './store'
-import App from './App.vue'
-import router from './router'
-
-
-Vue.use(VueRouter)
-Vue.use(Vuetify)
-
-new Vue({
-  el: '#app',
-  router,
-  store,
-  render: h => h(App)
-})
+console.log("Hello World")
--- src/frontend/store/mutations.js
+++
@@ -1,8 +0,0 @@
-export default {
-  ADD_RULE(state, rule) {
-    state.rules.push(rule)
-  },
-  SET_RULES(state, rules) {
-    state.rules = rules
-  }
-}
