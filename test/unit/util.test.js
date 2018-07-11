0c9708cf9e462b4041dbd79aa03ed529c2e7f58f
Travis Weidenbenner <travis@thetravisw.com>
2018-07-11 09:04:15 -0500

    style(tests): Fix linting in unit tests

--- test/unit/util.test.js
+++ test/unit/util.test.js
@@ -4,10 +4,10 @@ const util = require('../../lib/util/util')
 
 describe('Util module', () => {
   it('should add an increasing count number to every request', () => {
-    let countMiddleware = util.requestCount()
-    let req = {}
-    let res = {}
-    let next = jest.fn()
+    const countMiddleware = util.requestCount()
+    const req = {}
+    const res = {}
+    const next = jest.fn()
 
     countMiddleware(req, res, next)
 
@@ -24,26 +24,25 @@ describe('Util module', () => {
   })
 
   it('should filter hop by hop filters out of headers', () => {
-    let headers = {
+    const headers = {
+      connection: 'close, test-header',
+      te: 'testing',
+      trailer: 'trailer',
+      upgrade: 'upgrade',
       'content-type': 'application/json',
       'content-length': 348,
-      'connection': 'close, test-header',
-      'te': 'testing',
       'transfer-encoding': 'test1',
       'test-header': 'test-header',
       'keep-alive': 'keep-alive',
       'proxy-authorization': 'proxy-authorization',
-      'proxy-authentication': 'proxy-authentication',
-      'trailer': 'trailer',
-      'upgrade': 'upgrade'
+      'proxy-authentication': 'proxy-authentication'
     }
 
-    let filteredHeaders = util.filterResponseHeaders(headers)
+    const filteredHeaders = util.filterResponseHeaders(headers)
 
     // Headers should only contain content-type and content-length
     expect(Object.keys(filteredHeaders).length).toBe(2)
     expect(filteredHeaders['content-type']).toBe('application/json')
     expect(filteredHeaders['content-length']).toBe(348)
   })
-
 })
