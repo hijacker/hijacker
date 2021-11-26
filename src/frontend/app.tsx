118ebba3d6e15ccad91d144e77c0048cd0f4a184
Travis Weidenbenner <travis@thetravisw.com>
2021-11-26 17:54:51 -0600

    feat(rewrite): start rewrite from vue -> react
    Start of conversion to react in order to use typescript and be in controll of all deps
    
    BREAKING CHANGE: Going to be rewriting backend in typescript/and not incredibly worried about
    backwards compat

---
+++ src/frontend/app.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => ( 
+  <div>
+    <h1>Hello React</h1>
+    <div>Test 1</div>
+  </div>
+);
+
+export default App;
---
+++ src/frontend/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app.tsx";
+
+ReactDOM.render(<App />,document.querySelector("#root"));
