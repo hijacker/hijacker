bea5cd80d97268276c79fb020800357065ff6d39
Travis Weidenbenner <travis@weidenbenner.com>
2022-12-23 20:38:03 -0600

    add config validation on startup

--- packages/core/src/bin/importers/index.ts
+++ packages/core/src/bin/importers/index.ts
@@ -1,2 +1,9 @@
 export * from './json-importer.js';
 export * from './ts-importer.js';
+export * from './js-importer.js';
+
+export class ImportError extends Error {
+  constructor(message?: string, options?: ErrorOptions) {
+    super(message, options);
+  }
+}
